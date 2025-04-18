f76cc9a075ef10eef31b09f632bff6090977b3aa
Splashqq <149973359+Splashqq@users.noreply.github.com>
2025-04-19 02:49:27 +0300

    task_5: add user collections (#5)

--- medialibrary/users/admin.py
+++ medialibrary/users/admin.py
@@ -7,3 +7,21 @@ import medialibrary.users.models as users_m
 class UserAdmin(admin.ModelAdmin):
     list_display = ["id", "email", "username", "created_at"]
     readonly_fields = ["created_at", "updated_at"]
+
+
+@admin.register(users_m.UserMovieCollection)
+class UserMovieCollectionAdmin(admin.ModelAdmin):
+    list_display = ["user", "movie", "status"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+@admin.register(users_m.UserSeriesCollection)
+class UserSeriesCollectionAdmin(admin.ModelAdmin):
+    list_display = ["user", "series", "status"]
+    readonly_fields = ["created_at", "updated_at"]
+
+
+@admin.register(users_m.UserGameCollection)
+class UserGameCollectionAdmin(admin.ModelAdmin):
+    list_display = ["user", "game", "status"]
+    readonly_fields = ["created_at", "updated_at"]
